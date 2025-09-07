7ef43587e294e997bd27d61f1b947d4773aadd9b
Richard Gladman <richard@thefifthcontinent.com>
2025-09-07 08:59:46 +0100

    Implement file handler

---
+++ include/util/filehandler.h
@@ -0,0 +1,28 @@
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+
+#pragma once
+
+#include <string>
+#include <fstream>
+
+class FileHandler
+{
+public:
+    static void create_directories(std::string path);
+    static void create_data_file(std::string filename);
+
+    void open(const std::string &filename);
+    void write(const char *data, int length, const char mode = 'A');
+    void read(char *data, int length);
+    void close();
+
+    int get_pos();
+    bool eof();
+
+
+private:
+    std::fstream m_file;
+};
+
+#endif
---
+++ src/util/filehandler.cpp
@@ -0,0 +1,56 @@
+//
+// Created by richard on 28.08.2025 
+//
+#include "util/filehandler.h"
+#include <filesystem>
+#include <string>
+
+#include <iostream>
+
+void FileHandler::create_directories(std::string path)
+{
+    if (!std::filesystem::exists(path)) {
+        std::filesystem::create_directories(path);
+    }
+}
+
+void FileHandler::create_data_file(std::string filename)
+{
+    if (!std::filesystem::exists(filename)) {
+        std::ofstream file {filename};
+    }
+}
+
+void FileHandler::open(const std::string &filename)
+{
+    m_file.open(filename.data(), std::ios::binary | std::ios::in | std::ios::out);
+}
+
+void FileHandler::write(const char *data, int length, const char mode)
+{
+    if (mode == 'A') {
+        m_file.seekp(0, std::ios::end);
+    }
+
+    m_file.write(data, length);
+}
+
+void FileHandler::read(char *data, int length)
+{
+    m_file.read(data, length);
+}
+
+void FileHandler::close()
+{
+    m_file.close();
+}
+
+int FileHandler::get_pos()
+{
+    return m_file.tellp();
+}
+
+bool FileHandler::eof()
+{
+    return m_file.eof();
+}
